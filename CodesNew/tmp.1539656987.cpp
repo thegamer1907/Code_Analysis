c84fecec280c16f2f9ca098712f51482cf4be1df
Akash Ravi <akashkravi@gmail.com>
2018-10-16 08:27:47 +0530

    analyzed headers for 1000+ codes

---
+++ CodesNew/tmp.1539656987.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+#include <vector>
+
+#include <set>
+
+#include <algorithm>
+
+#include <math.h>
+
+#include <map>
+
+//#include <string>
+
+
+
+using namespace std;
+
+
+
+vector <int> a;
+
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+
+	cin.tie(NULL);
+
+
+
+	int n, m;
+
+	cin >> n;
+
+	cin >> m;
+
+	int mx = 0;
+
+	for (int i = 0; i < n; i++) {
+
+		int q;
+
+		cin >> q;
+
+		a.push_back(q);
+
+		if (mx < q) mx = q;
+
+	}
+
+	int mxx = mx + m;
+
+	sort(a.rbegin(), a.rend());
+
+	for (int i = 0; i < a.size(); i++) {
+
+		if (m <= 0) {
+
+			cout << mx << ' ' << mxx;
+
+			return 0;
+
+		}
+
+		m -= mx - a[i];
+
+		a[i] = mx;
+
+	}
+
+	if (m > 0)
+
+		mx += (m + n - 1) / n;
+
+	cout << mx << ' ' << mxx << endl;
+
+	
+
+
+
+	return 0;
+
+}
