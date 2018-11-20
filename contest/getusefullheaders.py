097fee9bdb5601173c4abe59fd9695c6b2533e0c
mukeshchugani10 <mukeshchugani10@gmail.com>
2018-11-20 14:04:46 +0530

    Completed Contest Codes

---
+++ contest/getusefullheaders.py
@@ -0,0 +1,36 @@
+def find_all_used_header(code,headers,positions,version=14):
+    '''
+    params:
+    code : string the complete code
+    headers : list of all headers
+    positions : starting indices of the headers recived in headers
+    version : By default version is 14 - supported version 14/11
+    Note : headers and positions are 1D array and len(headers) == len(positions) and also requires g++ compiler to be specified in the system path
+    '''
+    import subprocess
+    import time
+    import calendar
+    import os
+    import shutil
+    if len(headers) != len(positions):
+        raise Exception('len(headers) != len(positions but expected to be of same length')
+    if not os.path.exists('./tmp'):
+    	os.makedirs('./tmp')
+    m = {}
+    random_ = str(calendar.timegm(time.gmtime()))
+    file_name = './tmp/tmp.' + random_ + '.cpp'
+    exe_name = './tmp/tmp.' + random_ + '.exe'
+    for x,y in zip(headers,positions):
+        code_ = code[:y] + '//' + code[y:]
+        with open(file_name,'w') as cpp:
+            cpp.write(code_)
+            
+        try:
+            out = subprocess.check_output(['g++',file_name,'-o',exe_name])
+            m[x] = 0
+        except subprocess.CalledProcessError as out:                                                                                         
+            m[x] = 1
+            
+    shutil.rmtree('./tmp') 
+
+    return m
---
+++ contest/tmp/tmp.1542649868.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+ 
+
+ 
+
+using namespace std;
+
+
+
+//typedef long long LL;
+
+typedef unsigned long long ULL;
+
+typedef pair<int,int> PII;
+
+
+
+#define REP(i,n) for(int i=0;i<(n);++i)
+
+#define FOR(i,a,b) for (int i=(a); i<(b); ++i)
+
+#define FORD(i,a,b) for (int i=(a)-1; i>=(b); --i)
+
+
+
+#define pb push_back
+
+#define mp make_pair
+
+#define st first
+
+#define nd second
+
+
+
+const int MOD = 1000000007;
+
+
+
+char pass[5];
+
+char words[200][5];
+
+
+
+int main() {
+
+   
+
+
+
+  int N;
+
+  scanf("%s%d", pass, &N);
+
+  REP(i,N) scanf("%s", words[i]);
+
+
+
+  bool found = false;
+
+  REP(i,N) if (string(words[i]) == string(pass)) found = true;
+
+  REP(i,N) REP(j,N) if (words[i][1] == pass[0] && words[j][0] == pass[1]) found = true;
+
+  printf("%s\n", found ? "YES" : "NO");
+
+}
+
