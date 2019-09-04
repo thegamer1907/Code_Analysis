273e72f829dbe6ded8dbb1458549b347b49bd56c
Akash Ravi <akashkravi@gmail.com>
2019-09-04 17:22:12 +0530

    isolated pipeline scripts
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

---
+++ pipeline/comment_remover.py
@@ -0,0 +1,14 @@
+import re
+
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return " " # note: a space and not an empty string
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
